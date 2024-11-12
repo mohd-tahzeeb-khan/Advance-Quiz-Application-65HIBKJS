4363bbff6449f527e1318806669d0f01ee131f4b
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-12 23:55:28 +0530

    working on Header of the main page

--- quiz-app/src/app/components/header.js
+++ quiz-app/src/app/components/header.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import Link from 'next/link'
 const header = () => {
   return (
     <>
-    <div className='header w-full h-[10vh] bg-slate-500'>
+    <div className='header w-full h-[10vh] bg-slate-500
+    flex flex-row justify-between px-40'>
+        <div className='logo'></div>
+        <div className='links flex gap-5 items-center justify-center'>
+            <Link href={""}>Home</Link>
+            <Link href={""}>About</Link>
+            <Link href={""}>How to ?</Link>
+
+        </div>
+        <div className='loginbtn'>
+            <div className='signup'>Signup</div>
+            <div className='loginbtn'>Login</div>
+        </div>
 
 
     </div>
