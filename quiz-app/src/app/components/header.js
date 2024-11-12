952d9cc13e2cc89ee9c3f231ac334fef31bc9346
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-13 00:47:31 +0530

    working on header file

--- quiz-app/src/app/components/header.js
+++ quiz-app/src/app/components/header.js
@@ -3,18 +3,21 @@ import Link from 'next/link'
 const header = () => {
   return (
     <>
-    <div className='header w-full h-[10vh] bg-slate-500
-    flex flex-row justify-between px-40'>
-        <div className='logo'></div>
-        <div className='links flex gap-5 items-center justify-center'>
-            <Link href={""}>Home</Link>
-            <Link href={""}>About</Link>
-            <Link href={""}>How to ?</Link>
+    <div className='header w-full h-[8vh] bg-lightblue2 text-slate-700
+    flex flex-row justify-between px-10 py-5'>
+            <div className='logo min-w-[33%]'></div>
+        <div className='links min-w-[33%] 
+        flex gap-12 items-center justify-center'>
+            <Link className='hover:scale-105 hover:duration-200 text-2xl font-bold hover:text-slate-900' href={""}>Home</Link>
+            <Link className='hover:scale-105 hover:duration-200 text-2xl font-bold hover:text-slate-900' href={""}>About</Link>
+            <Link className='hover:scale-105 hover:duration-200 text-2xl font-bold hover:text-slate-900' href={""}>How to?</Link>
 
         </div>
-        <div className='loginbtn'>
-            <div className='signup'>Signup</div>
-            <div className='loginbtn'>Login</div>
+        <div className='loginbtn min-w-[33%]
+        flex gap-3 justify-end items-center
+        font-bold text-xl'>
+            <div className='signup    hover:scale-105 hover:duration-150 px-4 py-2 border-2'>Signup</div>
+            <div className='loginbtn  hover:scale-105 hover:duration-150 px-4 py-2 border-2  '>Login</div>
         </div>
 
 
