19179b4ff880e068d802ff37580821f2dde195c2
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-13 01:21:51 +0530

    Working on Footer  of the project

--- quiz-app/src/app/components/footer.js
+++ quiz-app/src/app/components/footer.js
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import WholeLogo from '../../../public/Images/full logo.png'
 import React from 'react'
-
 const footer = () => {
   return (
-    <div>footer</div>
+    <>
+    <div className='footer min-h-80 min-w-full
+    flex justify-between px-10 border-white bg-lightblue border-2'>
+        <div className='left min-w-[50%] my-auto mx-auto px-10'><Image src={WholeLogo} height={300} width={300} alt='logo'/></div>
+        <div className='right min-w-[50%]'></div>
+
+    </div>
+    
+    </>
   )
 }
 
