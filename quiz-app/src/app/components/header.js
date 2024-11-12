668b33bc6a690c123f83f8547631dfdeb1a24c86
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-12 23:49:57 +0530

    Added components

---
+++ quiz-app/src/app/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <>
+    <div className='header w-full h-[10vh] bg-slate-500'>
+
+
+    </div>
+    </>
+  )
+}
+
+export default header
