7607a7a3e0be032be949be5af3fe2234a6e694c1
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-13 01:21:21 +0530

    Working on Header of the project

--- quiz-app/src/app/components/header.js
+++ quiz-app/src/app/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Horizontalogo from '../../../public/Images/horizon logo.png'
 const header = () => {
   return (
     <>
     <div className='header w-full h-[8vh] bg-lightblue2 text-slate-700
     flex flex-row justify-between px-10 py-5'>
-            <div className='logo min-w-[33%]'></div>
+            <div className='logo min-w-[33%] p-0'><Image src={Horizontalogo} width={150} height={100} alt='logo' /></div>
         <div className='links min-w-[33%] 
         flex gap-12 items-center justify-center'>
             <Link className='hover:scale-105 hover:duration-200 text-2xl font-bold hover:text-slate-900' href={""}>Home</Link>
