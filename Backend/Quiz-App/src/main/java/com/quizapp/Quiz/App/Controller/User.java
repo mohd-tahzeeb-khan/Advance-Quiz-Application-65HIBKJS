17449c38c77cf6254ddfc721a70d995cb2eeff74
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-16 23:15:24 +0530

    New controller added to the project named user.java

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/User.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/User.java
@@ -1,2 +1,20 @@
-package com.quizapp.Quiz.App.Controller;public class User {
+package com.quizapp.Quiz.App.Controller;
+
+
+import com.quizapp.Quiz.App.Entity.user;
+import com.quizapp.Quiz.App.Repository.userRepo;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/user")
+public class User {
+
+@GetMapping("/create")
+    public String create() {
+    return "create";
+}
+
 }
