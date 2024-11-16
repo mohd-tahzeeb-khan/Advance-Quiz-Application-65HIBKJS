0aae879d1ca6dd5d43c60310328173e457737ca4
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-17 00:38:45 +0530

    altered Entity named result

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/result.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/result.java
@@ -1,4 +1,16 @@
 package com.quizapp.Quiz.App.Entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class result {
+
+    @Id
+    private String id;
 }
