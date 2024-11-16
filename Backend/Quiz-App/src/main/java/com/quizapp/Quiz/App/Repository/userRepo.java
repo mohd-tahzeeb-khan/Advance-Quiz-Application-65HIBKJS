5f4b9b140cb5bbd26949742d3cfcf050d241d21b
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-17 00:40:36 +0530

    Altered Repository name userRepo

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/userRepo.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/userRepo.java
@@ -1,2 +1,11 @@
-package com.quizapp.Quiz.App.Repository;public interface userRepo {
+package com.quizapp.Quiz.App.Repository;
+
+import com.quizapp.Quiz.App.Entity.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface userRepo extends JpaRepository<user, Long> {
+
 }
