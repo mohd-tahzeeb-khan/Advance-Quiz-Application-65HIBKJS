c2b37d7ac4f5af1db4a8acc1d0f4060360f37a67
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-17 00:41:49 +0530

    userService

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/userService.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/userService.java
@@ -1,2 +1,21 @@
-package com.quizapp.Quiz.App.Services;public class userService {
+package com.quizapp.Quiz.App.Services;
+
+
+import com.quizapp.Quiz.App.Entity.user;
+import com.quizapp.Quiz.App.Repository.userRepo;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class userService {
+
+    @Autowired
+    private userRepo userrepositoryinstance;
+
+
+    public Boolean CreateUser(@NotNull user userdata){
+        userrepositoryinstance.save(userdata);
+        return true;
+    }
 }
