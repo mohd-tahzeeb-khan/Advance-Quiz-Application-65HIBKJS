5a2dbcc430989c6784cc1ee1a7cd8ca56db7776c
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-18 23:18:58 +0530

    New file added to the Project folder named resultRepo.javba

---
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/resultRepo.java
@@ -0,0 +1,10 @@
+package com.quizapp.Quiz.App.Repository;
+
+import com.quizapp.Quiz.App.Entity.result;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+public interface resultRepo extends JpaRepository<result,Integer> {
+}
