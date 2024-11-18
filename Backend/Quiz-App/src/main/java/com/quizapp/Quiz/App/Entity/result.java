a621bde9b026b0c8136d58202a6ae691aa17992e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-18 23:15:12 +0530

    Modified Entity file named as Result.java

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/result.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/result.java
@@ -1,10 +1,12 @@
 package com.quizapp.Quiz.App.Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
 
 @Entity
 @Data
@@ -12,5 +14,13 @@ import lombok.NoArgsConstructor;
 public class result {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int resultId;
+
+    @NotNull
+    private float score;
+    @NonNull
+    private String userEmail;
+
+    private String mcqId;
 }
