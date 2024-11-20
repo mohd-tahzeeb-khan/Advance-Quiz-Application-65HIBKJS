623bd1eece3af2a259b716f3c46599a353d609f8
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-20 21:07:31 +0530

    ..

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/Exams.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/Exams.java
@@ -1,2 +1,27 @@
-package com.quizapp.Quiz.App.Controller;public class exams {
+package com.quizapp.Quiz.App.Controller;
+
+import com.quizapp.Quiz.App.Entity.exams;
+import com.quizapp.Quiz.App.Services.examService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("exam")
+public class Exams {
+
+    @Autowired
+    private examService examservice;
+
+    @PostMapping("/create/{email}")
+    public ResponseEntity<String> create(@PathVariable String email, @RequestBody exams examdata) {
+        boolean status=examservice.createExam(email, examdata);
+        return new ResponseEntity<>("created", HttpStatus.CREATED);
+    }
+    @GetMapping("/getexams/{email}")
+    public ResponseEntity<exams> getexams(@PathVariable String email) {
+        exams exam=examservice.GetExam(email);
+        return new ResponseEntity<>(exam, HttpStatus.OK);
+    }
 }
