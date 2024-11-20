67fa9c5885cbb10733d026ac526feb700f68a3f7
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-20 23:37:35 +0530

    added new Functionality in service class of the result

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/resultService.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/resultService.java
@@ -20,11 +20,40 @@ public class resultService {
     private userService userserviceinstance;
 
 
-    public void addresulttouser(@NonNull result resultdata){
-        result data=resultrepoinstance.save(resultdata);
-        user currentuser=userserviceinstance.Getuser(resultdata.getUserEmail());
-        currentuser.getResult().add(data);
-        userserviceinstance.Updateuser(currentuser);
+    public boolean addresulttouser(@NonNull String email,@NonNull result resultdata){
+        user currentuser=userserviceinstance.Getuser(email);
+        if(currentuser ==null){
+            return false;
+        }
+        else{
+            resultdata.setUser(currentuser);
+            result data=resultrepoinstance.save(resultdata);
+            return true;
+        }
+//        System.out.println("Quiery answers:"+currentuser);
+
+//        user currentuser=userserviceinstance.Getuser(resultdata.getUser_id().getEmail());
+//        currentuser.getResult().add(data);
+//        userserviceinstance.Updateuser(currentuser);
 
     }
+    public List<result> getuserresult(String email){
+        user currentuser=userserviceinstance.Getuser(email);
+        if(currentuser !=null){
+            return currentuser.getResult();
+
+        }
+        else {
+            return null;
+        }
+
+
+    }
+    public boolean deletebyid(int data){
+
+        resultrepoinstance.deleteById(data);
+        return true;
+    }
+
+
 }
