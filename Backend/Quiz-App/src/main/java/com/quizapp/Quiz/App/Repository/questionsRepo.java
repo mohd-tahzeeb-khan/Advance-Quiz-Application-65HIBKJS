37f2b2153238cef06fdaee06783e643a94df27e8
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-21 23:37:18 +0530

    Added new Repository file for Questions

---
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/questionsRepo.java
@@ -0,0 +1,10 @@
+package com.quizapp.Quiz.App.Repository;
+
+import com.quizapp.Quiz.App.Entity.questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface questionsRepo extends JpaRepository<questions, Integer> {
+}
