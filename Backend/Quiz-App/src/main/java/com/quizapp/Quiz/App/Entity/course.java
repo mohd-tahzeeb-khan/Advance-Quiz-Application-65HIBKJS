49a53a3548e1ab0db5cc34cf9c6c7c10f093da36
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-21 23:42:13 +0530

    altered Entity to store data

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/course.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/course.java
@@ -1,8 +1,33 @@
 package com.quizapp.Quiz.App.Entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @Column(nullable = false)
+    private String category;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Nullable
+    private List<exams> exams = new ArrayList<>();
 
 
 
