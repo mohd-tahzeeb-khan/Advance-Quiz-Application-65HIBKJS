61ae038d185729384b9c75c49df3786d33f7470b
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-21 23:39:13 +0530

    added new method to perform operations

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/courseRepo.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/courseRepo.java
@@ -1,2 +1,12 @@
-package com.quizapp.Quiz.App.Repository;public interface courseRepo {
+package com.quizapp.Quiz.App.Repository;
+
+import com.quizapp.Quiz.App.Entity.course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface courseRepo extends JpaRepository<course, Integer> {
+
+
 }
