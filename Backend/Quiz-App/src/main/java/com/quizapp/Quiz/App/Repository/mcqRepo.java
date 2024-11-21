31e8a3e0a88fd1a44bf69e56792591bccbeb1328
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-21 23:40:09 +0530

    added new method to perform operations

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/mcqRepo.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/mcqRepo.java
@@ -1,2 +1,9 @@
-package com.quizapp.Quiz.App.Repository;public interface mcqRepo {
+package com.quizapp.Quiz.App.Repository;
+
+import com.quizapp.Quiz.App.Entity.mcq_handler;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface mcqRepo extends JpaRepository<mcq_handler, Integer> {
 }
