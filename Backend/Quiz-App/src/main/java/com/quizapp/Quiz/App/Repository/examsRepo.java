f2a607cdf1bbb3a4d0d1eb52d596955f83f7e0ae
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-21 23:39:46 +0530

    added new method to perform operations

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/examsRepo.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/examsRepo.java
@@ -4,5 +4,5 @@ import com.quizapp.Quiz.App.Entity.exams;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface examsRepo extends JpaRepository<exams, Integer> {
-    public exams findByEmail(String Email);
+    //public exams findByEmail(String Email);
 }
