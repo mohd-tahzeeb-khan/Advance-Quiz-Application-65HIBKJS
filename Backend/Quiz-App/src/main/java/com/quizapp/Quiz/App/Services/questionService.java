0628defb3d91bae05358a64ba752c7d76eeba23e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-22 19:06:24 +0530

    added more methods in service to handle and tackle more functionalities

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/questionService.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/questionService.java
@@ -2,7 +2,9 @@ package com.quizapp.Quiz.App.Services;
 
 import com.quizapp.Quiz.App.Entity.mcq_handler;
 import com.quizapp.Quiz.App.Entity.questions;
+import com.quizapp.Quiz.App.Repository.answersRepo;
 import com.quizapp.Quiz.App.Repository.questionsRepo;
+import jakarta.servlet.MultipartConfigElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +21,19 @@ public class questionService {
     private mcqService mcqservice;
 
 
+    @Autowired
+    private answersRepo answersrepoinstence;
+    @Autowired
+    private MultipartConfigElement multipartConfigElement;
+
+
     public questions addquestions(int id, questions questions){
         Optional<mcq_handler> mcq_handler=mcqservice.getdatabyid(id);
         if(mcq_handler.isPresent()){
             mcq_handler mcq_handlerOptional=mcq_handler.get();
             questions.setMcq_handler(mcq_handlerOptional);
+
+//            answersrepoinstence.save(questions.getAnswers());
             return questionrepoinstence.save(questions);
         }
         throw new RuntimeException("Please Enter the data properly."+id);
