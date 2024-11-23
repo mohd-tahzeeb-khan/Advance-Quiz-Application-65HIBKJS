d29fa0adfe6d5f1b3d900d3f51f0f532667f8602
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-23 23:27:42 +0530

    Added new Request handler in Question cls

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/MCQuestion.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/MCQuestion.java
@@ -28,21 +28,24 @@ public class MCQuestion {
         this.mcqservice = mcqservice;
     }
 
+
     @PostMapping("/create")
     public ResponseEntity<mcq_handler> createMCQ(@RequestBody mcq_handler mcq) {
         mcq_handler savedMCQ = mcqservice.createmcq(mcq);
         return ResponseEntity.ok(savedMCQ);
     }
 
-
+// <---------This Mapping is used to add Questions in the Exam------------>
     @PostMapping("/addquestions/{id}")
-    public ResponseEntity<?> createMCQ(@PathVariable int id,@RequestBody questions mcq) {
-        questions addquestions=questionservice.addquestions(id, mcq);
+    public  ResponseEntity<?> createMCQ(@PathVariable int id, @RequestBody questions questions) {
+        mcq_handler reutrned=mcqservice.addanswer(id, questions.getAnswer()); //Writing(Storing) Answers to the mcq_handler
+        questions.setAnswer("");
+        questionservice.addquestions(id, questions); ////Writing(Storing) Questions and Options to the question table
+        return new ResponseEntity(reutrned, HttpStatus.OK);
 
-        return new ResponseEntity<>(addquestions, HttpStatus.OK);
     }
 
-
+    // <---------This Mapping is used to Alter(Update) Questions and Options in the Existing Questions------------>
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateMCQ(@NonNull @PathVariable int id, @RequestBody questions mcq) {
         questions responseupdate=mcqservice.updatedata(id, mcq);
@@ -53,14 +56,13 @@ public class MCQuestion {
     }
 
 
-
-
-    @GetMapping("getallmcq")
+// <---------This Mapping is used to See all the question in the Exam------------>
+    @GetMapping("/getallmcq")
     public ResponseEntity<List<mcq_handler>> getAllMCQs() {
         List<mcq_handler> mcqs = mcqservice.getdatabyall();
         return ResponseEntity.ok(mcqs);
     }
-
+    // <---------This Mapping is used to see(View) the specific question in the Exam------------>
     @GetMapping("/get/{id}")
     public ResponseEntity<?> getMCQById(@PathVariable int id) {
         Optional<mcq_handler> mcq = mcqservice.getdatabyid(id);
