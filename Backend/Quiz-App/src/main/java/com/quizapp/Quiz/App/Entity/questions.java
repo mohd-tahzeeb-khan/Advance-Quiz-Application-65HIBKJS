66d5d71132daf1997b6cee50d3bd5ba0bf44fc34
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-23 23:29:09 +0530

    altered the file

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/questions.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/questions.java
@@ -2,6 +2,7 @@ package com.quizapp.Quiz.App.Entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jdk.jfr.Enabled;
 import lombok.Data;
@@ -27,9 +28,9 @@ public class questions {
     @Column(name = "options")
     private List<String> options;
 
-    @OneToMany
-    @CollectionTable(name = "question_answer", joinColumns = @JoinColumn(name = "answers_id"))
-    private answers answers;
+
+    @Nullable
+    private String answer;
 
     @ManyToOne
     @JoinColumn(name = "mcq_id", nullable = false)
