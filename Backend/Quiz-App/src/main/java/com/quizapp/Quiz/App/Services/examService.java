3d69997bf16e63ab869b2bfd62d90a6064ae18f6
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-24 23:44:06 +0530

    Altered services in Exam Service

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/examService.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/examService.java
@@ -1,10 +1,13 @@
 package com.quizapp.Quiz.App.Services;
 
+import com.quizapp.Quiz.App.Controller.Course;
 import com.quizapp.Quiz.App.Controller.Result;
 import com.quizapp.Quiz.App.Controller.User;
+import com.quizapp.Quiz.App.Entity.course;
 import com.quizapp.Quiz.App.Entity.exams;
 import com.quizapp.Quiz.App.Entity.result;
 import com.quizapp.Quiz.App.Entity.user;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.quizapp.Quiz.App.Repository.examsRepo;
@@ -25,21 +28,52 @@ public class examService {
     @Autowired
     private resultService resultservice;
 
+    @Autowired
+    private courseService courseservice;
 
 
-    public exams createExam( String email, exams examdata){
+
+    public exams createExam(int id, String email, exams examdata){
         if(examdata !=null) {
-            user currentuser = userservice.Getuser(email);
-            List<result> currentuserresult = resultservice.getuserresult(email);
-            examdata.setDateCreate(LocalDateTime.now());
-            examdata.setCreator(email);
-            examdata.setUser(currentuser);
-            return examsRepo.save(examdata);
+            if(courseservice.isExist(id)){
+                Optional<course> getcourse=courseservice.getCourseById(id);
+                course course = getcourse.get();
+                user currentuser = userservice.Getuser(email);
+                    if(currentuser != null && course!=null){
+                        List<result> currentuserresult = resultservice.getuserresult(email);
+                        examdata.setDateCreate(LocalDateTime.now());
+                        examdata.setCreator(email);
+                        examdata.setUser(currentuser);
+                        examdata.setCourse(course);
+                        return examsRepo.save(examdata);
+                    }
+
+            }
+            else{
+                return null;
+            }
+
+
+
+
         }
         return null;
         }
     public exams getExam(int id){
         return examsRepo.findById(id).orElse(null);
+
+    }
+    public boolean deleteExam(int id){
+        if(examsRepo.existsById(id)){
+            examsRepo.deleteById(id);
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+
     }
     }
 
