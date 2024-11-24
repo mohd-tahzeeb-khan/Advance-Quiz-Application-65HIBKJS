67a6a644e8c42fff1fbb24fc43d1034377643338
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-24 23:45:45 +0530

    Testing

--- Backend/Quiz-App/src/test/java/com/quizapp/Quiz/App/QuizAppApplicationTests.java
+++ Backend/Quiz-App/src/test/java/com/quizapp/Quiz/App/QuizAppApplicationTests.java
@@ -1,10 +1,14 @@
 package com.quizapp.Quiz.App;
 
+import com.quizapp.Quiz.App.Controller.Exams;
+import com.quizapp.Quiz.App.Services.courseService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuizAppApplicationTests {
+
 
 	@Test
 	void contextLoads() {
