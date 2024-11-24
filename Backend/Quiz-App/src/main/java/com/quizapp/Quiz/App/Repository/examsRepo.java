517a6281a0bda661d874baecd9fe82436a5c2846
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-24 23:42:49 +0530

    added some methods in exams Repository

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/examsRepo.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Repository/examsRepo.java
@@ -2,7 +2,10 @@ package com.quizapp.Quiz.App.Repository;
 
 import com.quizapp.Quiz.App.Entity.exams;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface examsRepo extends JpaRepository<exams, Integer> {
     //public exams findByEmail(String Email);
 }
