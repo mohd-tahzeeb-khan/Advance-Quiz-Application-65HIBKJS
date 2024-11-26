fb3e19a018e3adc110cdf4f3a80b91104f647d98
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-26 23:20:25 +0530

    added new File(bean) for configuration

---
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Configuration/securityCheck.java
@@ -0,0 +1,61 @@
+package com.quizapp.Quiz.App.Configuration;
+
+
+import com.quizapp.Quiz.App.FIlter.jwtFilter;
+import com.quizapp.Quiz.App.Services.loaduser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class securityCheck {
+
+
+    @Autowired
+    private loaduser loaduserservice;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, jwtFilter jwtFilter) throws Exception {
+
+        return http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/user/**","/auth/**", "/course/**" ).permitAll()
+                        .requestMatchers("exam/**").authenticated()
+                        .requestMatchers("/examiner/**").hasRole("examiner")
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(loaduserservice).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
