9ae1b2c5938bcdaf2491c6f9237211581c9e2461
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-26 23:23:50 +0530

    created new service class to send mails to the user for recreating password

---
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/mailsender.java
@@ -0,0 +1,32 @@
+package com.quizapp.Quiz.App.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class mailsender {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String body){
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom("quizapp");
+        mailSender.send(message);
+    }
+
+
+    public void otpMail(String to, String subject, String body){
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom("quizapp");
+        mailSender.send(message);
+    }
+}
