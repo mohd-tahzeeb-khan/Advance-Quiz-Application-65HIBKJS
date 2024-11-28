8b7ed1beebe7f3de72866d642f5c1993f84b320a
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-28 23:08:54 +0530

    Working on Page.js file of mcQuestions

--- quiz-app/src/app/mcQuestions/page.js
+++ quiz-app/src/app/mcQuestions/page.js
@@ -4,13 +4,16 @@ import React, { useState, useEffect } from 'react';
 
 const QuestionsPage = () => {
   const [questions, setQuestions] = useState([]);
-
+  const [exams, setexams] = useState([]);
   useEffect(() => {
     const fetchQuestions = async () => {
-      const res = await fetch('http://localhost:8080/course/getall'); // Replace with your Spring Boot API URL
+      const res = await fetch("http://localhost:8080/course/getall"); // Replace with your Spring Boot API URL
       const data = await res.json();
       setQuestions(data);
+      console.log("");
       console.log(data);
+      setexams(data[0].exams);
+
     };
 
     fetchQuestions();
@@ -23,6 +26,11 @@ const QuestionsPage = () => {
         {questions.map((question) => (
           <li key={question.id}>{question.id}{question.name}{question.category}</li> // Adjust field names based on your Question model
         ))}
+        {
+          exams.map((exam)=> (
+            <li key={exam.exam_id}>{exam.creator}{exam.title}{exam.expiredate}{exam.total_marks}{exam.description}</li>
+          ))
+        }
       </ul>
     </div>
   );
