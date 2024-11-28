cce1a7e35e863d8187e2a7c5417a9b80001d8a54
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-28 23:12:22 +0530

    added new urls in the file

--- quiz-app/next.config.mjs
+++ quiz-app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+// next.config.mjs
 
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/",
+    },
+ 
+};
+  
+  
 export default nextConfig;
