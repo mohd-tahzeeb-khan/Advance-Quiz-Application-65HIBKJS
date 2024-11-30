c6352016173153a7c1fcf13c3e061c78a4f998fb
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-30 21:54:13 +0530

    added some configuration

--- quiz-app/next.config.mjs
+++ quiz-app/next.config.mjs
@@ -3,9 +3,18 @@ const nextConfig = {
 // next.config.mjs
 
     reactStrictMode: true,
-    env: {
-      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/",
-    },
+    // env: {
+    //   NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/",
+    // },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:8080/:path*', // Proxy to backend
+        },
+      ];
+    }
+    
  
 };
   
