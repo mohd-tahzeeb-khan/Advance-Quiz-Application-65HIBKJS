eb3f185b854683970b507163c6d932bd5c4d3637
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-01 23:42:11 +0530

    working on Exams Module to fetch data for the whole page

--- quiz-app/src/app/Exams/[examid]/page.js
+++ quiz-app/src/app/Exams/[examid]/page.js
@@ -8,7 +8,7 @@ export default function Page() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWRkdXJhYWpAZ21haWwuY29tIiwiaWF0IjoxNzMyOTg5NjA0LCJleHAiOjE3MzI5OTMyMDR9.6hExcXbeUXS4rbc0NK94QSSnca6zRkHt51Uy8NI-Ugw";
+  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWRkdXJhYWpAZ21haWwuY29tIiwiaWF0IjoxNzMzMDUzMTY5LCJleHAiOjE3MzMwNTY3Njl9.0kYkfwDaZt2AFJeEVTnMFwxy_zKWKd9plReoeQjF1fA";
   const { examid } = useParams();  // Get examid from URL params
   console.log('Exam ID:', examid);
 
