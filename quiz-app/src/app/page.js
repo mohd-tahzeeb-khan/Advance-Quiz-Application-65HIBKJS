1b7520c37581c4c90d2c1337ab5d569737857ac2
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-03 23:29:52 +0530

    quiz-app/src/app/Courses/[id]/

--- quiz-app/src/app/page.js
+++ quiz-app/src/app/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Home from './Home/page';
 const page = () => {
   return (
     <>
-      <Header />
-      <Footer />
+      
+      <Home />
+      
     </>
   )
 }
