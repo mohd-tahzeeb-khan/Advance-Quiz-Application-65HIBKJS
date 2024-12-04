841c1de8af737fdc2df0090b0c2e1a4fcf85fd2c
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-04 23:10:25 +0530

    Edited the Entity for Examiner

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/examiner.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/examiner.java
@@ -1,4 +1,59 @@
 package com.quizapp.Quiz.App.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "exminer")
 public class examiner {
+
+
+    @Id
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable=false)
+    private String role;
+
+    @Column(nullable = false)
+    private Character gender;
+
+    @Column(nullable = false)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private int zip;
+
+    @Column(nullable = true)
+    private int otp=0;
+
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<result> result = new ArrayList<>();
+    //
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<exams> exams = new ArrayList<>();
+//    @Column(nullable = false)
+//    @OneToMany(mappedBy = "exam_id")
+//    private List<exams> exams=new ArrayList<>();
+
 }
