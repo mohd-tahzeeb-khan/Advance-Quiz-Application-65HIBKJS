812b22506a3a75641a22d40f7188f68a3a507753
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-04 23:06:33 +0530

    Created new Controller for Examiner

---
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Services/examinerService.java
@@ -0,0 +1,40 @@
+package com.quizapp.Quiz.App.Services;
+
+
+import com.quizapp.Quiz.App.Entity.examiner;
+import com.quizapp.Quiz.App.Entity.user;
+import com.quizapp.Quiz.App.Repository.examinerRepo;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class examinerService {
+
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private examinerRepo examinerrepo;
+
+    public boolean createExaminer(examiner examiner) {
+        examiner.setPassword(passwordEncoder.encode(examiner.getPassword()));
+        examiner.setRole("Examiner");
+        examinerrepo.save(examiner);
+        return true;
+    }
+
+    public examiner getExaminer(@NotNull String email) {
+            examiner examiner = examinerrepo.findByEmail(email);
+            System.out.println("herer  "+examiner);
+            if(examiner == null) {
+                return null;
+            }else{
+                return examiner;
+            }
+    }
+}
