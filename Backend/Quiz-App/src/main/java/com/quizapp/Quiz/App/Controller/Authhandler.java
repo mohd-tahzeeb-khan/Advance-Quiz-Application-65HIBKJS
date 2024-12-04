1d66051490df062b7abe91606b1a3d42a2475e0d
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-04 23:14:55 +0530

    Edited the Controller class for Auth-Handler

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/Authhandler.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Controller/Authhandler.java
@@ -1,6 +1,8 @@
 package com.quizapp.Quiz.App.Controller;
 
+import com.quizapp.Quiz.App.Entity.examiner;
 import com.quizapp.Quiz.App.Entity.user;
+import com.quizapp.Quiz.App.Services.examinerService;
 import com.quizapp.Quiz.App.Services.userService;
 import com.quizapp.Quiz.App.Utilies.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,8 @@ public class Authhandler {
     @Autowired //Annotation.
     private userService userservice; //Autowired userService class from Service.
     @Autowired
+    private examinerService examinerservice;
+    @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserDetailsService userdetailsservice;
@@ -34,11 +38,11 @@ public class Authhandler {
 // <--------------------------------------------------------------------------------------------------------------->
 
     @PostMapping("/signup/examiner")
-    public ResponseEntity<?> signupExaminer(@RequestBody user user) {
-        if(user!=null) {
-            user usergetting=userservice.Getuser(user.getEmail());
-            if(usergetting==null) {
-                if(userservice.CreateUser(user)){
+    public ResponseEntity<?> signupExaminer(@RequestBody examiner examiner) {
+        if(examiner!=null) {
+           examiner examinerget=examinerservice.getExaminer(examiner.getEmail());
+            if(examinerget==null) {
+                if(examinerservice.createExaminer(examiner)){
                     return new ResponseEntity<>(HttpStatus.CREATED);
                 }
                 else{
@@ -92,6 +96,20 @@ public class Authhandler {
         }
 }//Method end -->loginUser
 
+    @PostMapping("/login/examiner")
+    public ResponseEntity<?> loginExaminer(@RequestBody examiner examiner) {
+        try {
+            System.out.println(examiner.getEmail() + " " + examiner.getPassword() );
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(examiner.getEmail(), examiner.getPassword()));
+            UserDetails userDetailsexam= userdetailsservice.loadUserByUsername(examiner.getEmail());
+            System.out.println(userDetailsexam);
+            String jwt=jwtUtil.generateToken(examiner.getEmail());
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    } //Method End -->loginExaminer
+
     @PostMapping("/login/user/autho")
     public ResponseEntity<?> loginAuthOUser(@RequestBody user user) {
         try {
@@ -105,17 +123,20 @@ public class Authhandler {
     }
 // <--------------------------------------------------------------------------------------------------------------->
 
-
-    @PostMapping("/login/examiner")
-    public ResponseEntity<?> loginExaminer(@RequestBody user user) {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
-            UserDetails userDetails= userdetailsservice.loadUserByUsername(user.getEmail());
-            String jwt=jwtUtil.generateToken(user.getEmail());
-            return new ResponseEntity<>(jwt,HttpStatus.OK);
-        }catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }
-    } //Method End -->loginExaminer
 // <--------------------------------------------------------------------------------------------------------------->
+    @PostMapping("/login/check/{jwt}")
+    public boolean checkjwt(@PathVariable String jwt){
+        try {
+            if(jwtUtil.isTokenExpired(jwt)){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
 }
