1b17e51ea0a6ba5905dd6e615a9f4eda4b2e25fc
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-12-21 23:44:17 +0530

    Altered the code for Entity class of the Course

--- Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/course.java
+++ Backend/Quiz-App/src/main/java/com/quizapp/Quiz/App/Entity/course.java
@@ -5,9 +5,39 @@ import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
+
+//@Entity
+//@Data
+//@NoArgsConstructor
+//public class course {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//
+//    @Column(nullable = false)
+//    private String name;
+//
+//
+//    @Column(nullable = false)
+//    private String category;
+//
+//    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @Nullable
+//    @ToString.Exclude
+//    private List<exams> exams = new ArrayList<>();
+//
+//
+//    @ManyToOne
+//    @JoinColumn(name = "examiner", nullable = false)
+//    @JsonIgnore
+//    private examiner examiner_course;
+//}
 
 @Entity
 @Data
@@ -18,18 +48,16 @@ public class course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
     @Column(nullable = false)
     private String name;
-
 
     @Column(nullable = false)
     private String category;
 
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
     @Nullable
+    @ToString.Exclude
     private List<exams> exams = new ArrayList<>();
-
 
     @ManyToOne
     @JoinColumn(name = "examiner", nullable = false)
